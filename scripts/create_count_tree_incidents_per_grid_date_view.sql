c9dc09681a11f16b669df90201f9db05a4215d70
Mladen Mladenov <mladen.mladenov@tumba.solutions>
2024-01-19 14:09:37 +0100

    adding scripts for views. registering views in admin.

--- scripts/create_count_tree_incidents_per_grid_date_view.sql
+++ scripts/create_count_tree_incidents_per_grid_date_view.sql
@@ -4,7 +4,8 @@ ipag.grid_id,
 ipsd.date,
 extract(HOUR from ipsd.incident_start_time) as hour,
 sum(ipsd.incident_duration) as total_incident_duration,
-max(ipsd.incident_priority) as max_incident_priority
+max(ipsd.incident_priority) as max_incident_priority,
+count(*) as count_incidents
 from incident_predictions_storm_damage as ipsd
 join public.incident_predictions_amsterdam_grid as ipag
 on st_contains(ipag.geometry, ipsd.geometry)
