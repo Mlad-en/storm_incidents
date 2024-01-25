8f52e5e52f684402fafaed68698dc0b0b69b36c1
Mladen Mladenov <mladen.mladenov@tumba.solutions>
2024-01-25 14:23:05 +0100

    updating views to use materialize css.

--- storm_incidents/settings.py
+++ storm_incidents/settings.py
@@ -129,6 +129,11 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    BASE_DIR / "static/incident_predictions"
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
